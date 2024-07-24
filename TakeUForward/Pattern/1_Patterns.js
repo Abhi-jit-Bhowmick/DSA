a8a3459c579e72f58632bf758f7959ef8cfe6711
Abhijit Bhowmick <bumba.abhi2012@gmail.com>
2024-07-24 23:47:34 +0530

    pattern_1

---
+++ TakeUForward/Pattern/1_Patterns.js
@@ -0,0 +1,31 @@
+/**
+ 
+    PRINT THE PATTERN
+
+    ****
+    ****
+    ****
+    ****
+
+
+*/
+
+
+function pattern1(row, collumn) {
+
+    // Outer Loop
+    for (let i = 0; i < row; i++) {
+
+        //inner loop
+        let ans = ""
+
+        for (let j = 0; j < collumn; j++) {
+            ans += "*"
+        }
+        console.log(ans)
+
+    }
+}
+
+
+pattern1(4, 4)
